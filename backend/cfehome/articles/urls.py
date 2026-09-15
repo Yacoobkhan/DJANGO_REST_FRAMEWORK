1f484109d1da40fce058e3317997f54ceaf52de7
Yacoob <yacoobkhan18@gmail.com>
2026-09-15 12:46:12 +0530

    implemented create,update and delete  in Articles backend.

--- backend/cfehome/articles/urls.py
+++ backend/cfehome/articles/urls.py
@@ -6,4 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.ArticleListView.as_view(), name='article-list'),
     path('<int:pk>/', views.ArticleDetailView.as_view(), name='article-detail'),
+    path('create',views.ArticleCreateView.as_view(),name='article-create'),
+    path('<int:pk>/update',views.ArticleUpdateView.as_view(),name='article-update'),
+    path('<int:pk>/destroy',views.ArticleDestroyView.as_view(),name='article-destroy'),
 ]
--- backend/cfehome/articles/views.py
+++ backend/cfehome/articles/views.py
@@ -11,3 +11,26 @@ class ArticleListView(generics.ListAPIView):
 class ArticleDetailView(generics.RetrieveAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+
+class ArticleCreateView(generics.CreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+    def perform_create(self,serializer):
+        serializer.save(user=self.request.user)
+
+class ArticleUpdateView(generics.UpdateAPIView):
+    queryset= Article.objects.all()
+    serializer_class = ArticleSerializer
+
+    def get_queryset(self):
+        return Article.objects.filter(user=self.request.user)
+
+class ArticleDestroyView(generics.DestroyAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+    def get_queryset(self):
+        return Article.objects.filter(user=self.request.user)
+    
+    
